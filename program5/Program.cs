29dd643323527cce44bb9182dfb2f37ab425fe3f
Kravchenko-Dmytro <dmitrokrav01@gmail.com>
2020-09-15 20:41:06 +0300

    s

---
+++ program5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+namespace program1
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hello World");
+
+        }
+
+    }
+}
